757855f89b828aab627ed844fa6da79ef6e2252a
liu-baoding <52502863+liu-baoding@users.noreply.github.com>
2025-12-12 15:35:38 +0000

    Deploying to gh-pages from @ liu-baoding/nav@d964928cb671746a04ddb55fb5489d289e6ba811 🚀

---
+++ chunk-B3NXRLHV.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-5CGUG6Y4.js";import{qb as r,zb as e}from"./chunk-E6P7GPOT.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
